b7c28a647e21e604163dd98db5c695072a06088f
Laurens <laurens.aarnoudse@gmail.com>
2018-07-07 20:04:47 +0200

    Disabled /register route

--- auth.js
+++ auth.js
@@ -21,23 +21,7 @@ getToken = function (headers) {
 };
 
 router.post('/register', function(req, res) {
-  //console.log(req.body)
-
-  if (!req.body.username || !req.body.password) {
-    res.json({success: false, msg: 'Please pass username and password.'});
-  } else {
-    var newUser = new User({
-      username: req.body.username,
-      password: req.body.password
-    });
-    // save the user
-    newUser.save(function(err) {
-      if (err) {
-        return res.json({success: false, msg: 'Username already exists.'});
-      }
-      res.json({success: true, msg: 'Successful created new user.'});
-    });
-  }
+  console.log("Post to register was attempted but no new users are accepted at this time")
 });
 
 router.post('/login', function(req, res) {
